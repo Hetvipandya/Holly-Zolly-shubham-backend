81181fcd0f07456f52994cd270dee12622123b1c
Hetvi_Pandya <pandyahetvi121@gmail.com>
2026-05-06 18:30:20 +0530

    all file add

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -1,9 +1,9 @@
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: process.env.domq86row,
-  api_key: process.env.CLOUDINARY_376633546122745,
-  api_secret: process.env.CLOUDINARY_OJtzJi7RbDP7ZBQ52uOwoU1-X0U,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
 });
 
